432277d46d337520f0f18475b83f5489296793fe
S Hobson <shobson@mailo.com>
2025-03-01 17:12:59 -0600

    playing with ui

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,25 @@
-﻿namespace DaggerfallUnityModManager.ViewModels
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using DynamicData;
+
+namespace DaggerfallUnityModManager.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
         public string Greeting { get; } = "Welcome to Avalonia!";
+
+        public ObservableCollection<ModEntry> Mods { get; set; }
+
+        public MainWindowViewModel()
+        {
+            var exampleMods = new List<ModEntry>
+            {
+                new("ex1", "ex1", false, 0),
+                new("ex2", "asfd", true, 1),
+                new("ex3", "qwerty", false, 2),
+            };
+
+            Mods = new ObservableCollection<ModEntry>(exampleMods);
+        }
     }
 }
---
+++ ViewModels/ModEntry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaggerfallUnityModManager.ViewModels
+{
+    public class ModEntry
+    {
+        public String Name { get; }
+        public String Filename { get; }
+        public Boolean IsEnabled { get; set; }
+        public int LoadPriority {  get; set; }
+
+        public ModEntry(String name, String filename, Boolean isEnabled, int loadPriority)
+        {
+            Name = name;
+            Filename = filename;
+            IsEnabled = isEnabled;
+            LoadPriority = loadPriority;
+        }
+    }
+}
